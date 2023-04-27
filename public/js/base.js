96f973ae2f17c5334e3e1079a4fdcce34bdcfcc1
Y-wo <y-wo@gmx.de>
2023-04-27 20:19:54 +0200

    - optic changes - some twig templates new

--- public/js/base.js
+++ public/js/base.js
@@ -1,16 +1,3 @@
-
-import {apiPath, localPath, remotePath } from "./configuration.js";
-
-// export async function checkAccess(){
-//     let jwtResponse = await checkJwtStatus()
-//     let isUserAuthentified = await jwtResponse.text()
-//     return isUserAuthentified === "true"
-// }
-
 document.addEventListener("DOMContentLoaded", async function(){
 
 })
-
-
-
-
